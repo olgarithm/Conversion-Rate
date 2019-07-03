9f314c8eb00e1ab0529c038e7a1c88360da9fefc
Olga Andreeva <andreolg@cs.washington.edu>
2019-07-03 17:51:10 +0200

    Add Swedish Krona as well

--- conversion.js
+++ conversion.js
@@ -3,10 +3,17 @@
 	window.addEventListener("load", init);
 
 	function init() {
-		id("submit").addEventListener("click", convertCurrency);
+		id("convert").addEventListener("click", convertKroner);
+		id("clear").addEventListener("click", function() {
+			clearInput("dollar", "danish")
+		});
+		id("convert-second").addEventListener("click", convertKrona);
+		id("clear-second").addEventListener("click", function() {
+			clearInput("dollar-second", "swedish");
+		})
 	}
 
-	function convertCurrency() {
+	function convertKroner() {
 		if (id("danish").value) {
 			id("dollar").value = parseInt(id("danish").value) / 6.60859;
 		} else {
@@ -14,7 +21,20 @@
 		}
 	}
 
+	function convertKrona() {
+		if (id("swedish").value) {
+			id("dollar-second").value = parseInt(id("swedish").value) / 9.31384;
+		} else {
+			id("swedish").value = parseInt(id("dollar-second").value) * 9.31384;
+		}
+	}
+
 	function id(elementId) {
 		return document.getElementById(elementId);
+	}
+
+	function clearInput(firstInput, secondInput) {
+		id(firstInput).value = "";
+		id(secondInput).value = "";
 	}
 })();
